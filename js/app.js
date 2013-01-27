8d8e7466f4d9195b3edfd15ef1b542095285683a
Tosh Shimayama <tshimayama@fujifilm.com>
2013-01-28 08:18:14 +0900

    fixed convergence problem

--- js/app.js
+++ js/app.js
@@ -1,11 +1,130 @@
 // handle missing window.console.log cases.
 var app = angular.module('app', []);
 
+var hist = function(data, min, binsize, N) {
+  var bin = [], inc = 1.0 / data.length;
+	var min1 = min - binsize/2;
+	for (var i = 0; i < N; i++)  // initialize
+		bin[i] = [min1 + i*binsize, 0];
+	angular.forEach(data, function(v) {
+		var index = Math.floor( ( v - min1 ) / binsize );
+		index = ( index < 0 ? 0 : index );
+		index = ( index > N-1 ? N-1 : index );
+		bin[index][1] += inc;
+	});
+	//console.log( bin );
+	return bin;
+};
+
+
+var D = 1, gconst = 1 / Math.pow( Math.sqrt(2 * Math.PI), D );
+var g = function( v, m, s ) {
+	return gconst / Math.pow(s, D) * Math.exp( -0.5 * Math.pow((v - m)/s, 2) );
+};
+
 app.controller('MainCtrl', function( $scope ) {
+
   $scope.data = [
 3937, 4360, 4391, 3422, 3313, 3859, 3406, 3328, 3375, 3859, 3813, 3328, 4422, 3875, 3891, 3859, 3844, 4359, 4344, 3922, 3859, 3843, 3421, 3921, 3765, 3407, 3875, 3812, 4359, 3422, 3406, 4391, 3500, 4312, 3344, 3359, 3891, 3891, 3843, 4312, 3781, 3922, 4281, 3422, 3344, 3906, 3344, 4328, 3422, 4594, 4235, 3891, 3875, 3390, 3875, 3359, 3407, 3875, 3344, 4406, 3406, 3453, 3359, 3343, 3375, 3860, 3812, 4344, 4265, 3391, 3406, 3906, 3859, 4391, 3797, 4359, 3906, 3938, 3890,
 3343, 3860, 3422, 3859, 3359, 3438, 3344, 3313, 4406, 3828, 3844, 3859, 4390, 3906, 4375, 4109, 3922, 4594, 4407, 3891, 3344, 3828, 4328, 3875, 3328, 3375, 3453, 3359, 3343, 3812, 4000, 3906, 3313, 3391, 4328, 3375, 3890, 3360, 3891, 3453, 3875, 3860, 3843, 3375, 3860, 4313];
 
-  $scope.res = EM($scope.data, 3, [3400, 3900, 4400], [30,30,30], [1,1,1]);
+  $scope.binsize = 50;
+
+	$scope.update_hist = function(binsize) {
+		var min = $scope.data[0], max = min, N, k;
+		angular.forEach($scope.data, function(v) {
+			if ( v < min ) min = v;
+			if ( max < v ) max = v;
+		});
+		console.log([min,max]);
+		N = 5 + Math.floor( ( max - min ) / binsize );
+		var h = hist($scope.data, min - 2 * binsize, binsize, N);
+		var options = {
+			series: {
+				bars: { barWidth: binsize }
+				//lines: { show: true },
+				//points: { show: true }
+			}
+		};
+		var gg = [], means = [];
+		if ($scope.result) {
+			// Gaussian mixture graph
+			var dist = $scope.result;
+			for (var x = min-binsize; x < max+binsize; x+=binsize/10) {
+				var f = 0.0;
+				for (k=0; k < $scope.result.m.length; k++) {
+					f += dist.p[k] * g(x, dist.m[k], dist.s[k]);
+				}
+				gg.push([x, binsize * f]);
+			}
+			// means
+			for (k=0; k < dist.m.length; k++) {
+				means[k] = [dist.m[k], 0];
+			}
+		}
+		var graphdata = [{data: h, bars: {show:true}}, gg, {points:{show:true}, data:means} ];
+		$.plot($("#placeholder"), graphdata, options);
+	};
+
+	$scope.$watch('binsize', function(binsize) {
+		$scope.update_hist(binsize);
+	});
+	$scope.$watch('data', function(data) {
+		$scope.result = EM($scope.data, 3, [3500, 4000, 4500], [30,30,30], [1,1,1]);
+		$scope.update_hist($scope.binsize);
+	});
+
+  //$scope.result = EM($scope.data, 3, [3500, 4000, 4500], [30,30,30], [1,1,1]);
+  $scope.files = [];
+
+  $scope.$watch( 'filelist', function( filelist ) {
+    if ( !filelist ) return;
+    $scope.files = [];
+    console.log( filelist );
+		var onload = function( event ) {
+			var text = event.target.result;
+			$scope.data = text.split(/[#\s]+/)
+				.map(function(v) {return +v;})
+				.filter(function(v) {return v > 0;});
+			$scope.$apply();
+		};
+    for ( var i = filelist.length - 1; i >= 0; i-- ) {
+      var file = filelist[i];
+			var reader = new FileReader();
+			reader.onload = onload;
+			reader.readAsText( file );
+      $scope.files.push( file );
+    }
+  }, true);
 });
 
+app.directive('dropArea', function() {
+  return function( scope, elm, attrs ) {
+    elm.bind("dragover", function( event ) {
+      event.stopPropagation();
+      event.preventDefault();
+    });
+    elm.bind("drop", function( event ) {
+      event.stopPropagation();
+      event.preventDefault();
+      scope.$apply(function( scope ) {
+        console.log( event.originalEvent.dataTransfer.files );
+        scope[ attrs.dropArea ] = event.originalEvent.dataTransfer.files;
+      });
+    });
+    elm.bind('click', function() {
+      $('input').click();
+    });
+  };
+});
+
+app.directive("filelistBind", function() {
+  return function( scope, elm, attrs ) {
+    elm.bind("change", function( evt ) {
+      //console.log( evt );
+      scope.$apply(function( scope ) {
+        scope[ attrs.name ] = evt.target.files;
+      });
+    });
+  };
+});
--- js/em.js
+++ js/em.js
@@ -74,24 +74,45 @@ var EM = function( x, K, m0, s0, p0 ) {
 	}
 
   // difference evaluation func
-	var diff = function(m0, m1) {
-		var k, sqsum = 0;
+	var diff = function(m0, s0, p0, m1, s1, p1) {
+		var k, sqsum_m = 0, sqsum_s = 0, sqsum_p = 0;
 		for (k = 0; k < K; k++) {
-			sqsum += ( m0[k] - m1[k] ) * ( m0[k] - m1[k] );
+			sqsum_m += ( m0[k] - m1[k] ) * ( m0[k] - m1[k] );
+			sqsum_s += ( s0[k] - s1[k] ) * ( s0[k] - s1[k] );
+			sqsum_p += ( p0[k] - p1[k] ) * ( p0[k] - p1[k] );
 		}
-		console.log( sqsum );
-		return Math.sqrt( sqsum );
+		console.log( sqsum_m, sqsum_s, sqsum_p );
+		return Math.sqrt( sqsum_m + sqsum_s );
+	};
+
+	var LogLikelihood = function(m, s, p) {
+		var n, k, prod_pkg = 1.0, sum_pkg;
+		for (n = 0; n < N; n++) {
+			sum_pkg = 0.0; 
+			for (k = 0; k < K; k++) {
+				sum_pkg += p[k] * g(x[n], m[k], s[k]);
+				//var aa = p[k] * g(x[n], m[k], s[k]);
+				//console.log([k, n, p[k], x[n], m[k], s[k]], aa, sum_pkg);
+				if (!sum_pkg) return;
+			}
+			prod_pkg += Math.log( sum_pkg );
+		}
+		return prod_pkg;
 	};
 
 	// iteration
-	for (i=0; i<10000; i++) {
+	var L_old;
+	for (i=0; i<10; i++) {
 		Expectation(p, m, s);
-		var res = Maximization();
-		if (diff(m, res.m) < 10) break;
+		var L = LogLikelihood(m, s, p),
+			res = Maximization();
+		console.log(i, " LogLikelihood: ", LogLikelihood(m, s, p));
+		if ( !isFinite(L) || Math.abs(L - L_old) < 1 ) break;
+		L_old = L;
 		p = res.p;
 		m = res.m;
 		s = res.s;
-		console.log([i, m]);
+		//console.log([i, m]);
 	}
 	return {m:m, s:s, p:p};
 };
