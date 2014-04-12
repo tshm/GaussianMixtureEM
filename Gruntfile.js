8ec02124e6b95e3074a76324a42c92bb71b99e92
tshm <2sm@csc.jp>
2014-04-12 23:21:22 +0900

    add dist / npm update

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ var mountFolder = function (connect, dir) {
 
 module.exports = function (grunt) {
   // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').concat(['gruntacular']).forEach(grunt.loadNpmTasks);
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // configurable paths
   var yeomanConfig = {
